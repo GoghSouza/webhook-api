d79f72f5fe53d4a32eac427aabbdb6fa3ee0593f
GoghSouza <mateus.silva@ticto.com.br>
2025-05-28 10:43:08 -0300

    API para Webhook

---
+++ app.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+import psycopg2
+from flask import Flask, request, jsonify
+import bcrypt
+from flask_caching import Cache
+
+app = Flask(__name__)
+
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 86400})
+
+@app.route('/teste', methods=['GET']) 
+def teste():
+    return jsonify({"message": "Hello World"}), 200
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    body = request.json
+    cache.set(webhook, (body))
+    return jsonify(body), 200
+
+@app.route('/webhook/consulta', methods=['GET'])
+def consulta():
+    return jsonify(cache.get(webhook))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+#
